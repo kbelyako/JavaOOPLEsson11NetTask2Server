8503e3df3c6432ee3a20010f39e8197c2dd8cf40
Kir <kbelyako@yandex.com>
2019-09-09 00:15:15 +0600

    correcred

--- JavaOOPLesson11NetTask2WebServer/src/com/yandex/kbelyako/Main.java
+++ JavaOOPLesson11NetTask2WebServer/src/com/yandex/kbelyako/Main.java
@@ -9,28 +9,27 @@ public class Main {
 		String answer = "";
 
 		answer += "<html><head><title>Server</title> <meta charset='utf-8'></head><body><p>System information: "
-				 + "</p><br>";		
+				+ "</p><br>";
 		answer += "<body>";
-		answer += "<p>" +"Total memory (bytes): " +  Runtime.getRuntime().totalMemory() + "</p>";
-		answer += "<p>" +"Free memory (bytes): " +  Runtime.getRuntime().freeMemory() + "</p>";
-		answer += "<p>" +"available Processors : " +  Runtime.getRuntime().availableProcessors() + "</p>";
+		answer += "<p>" + "Total memory (bytes): " + Runtime.getRuntime().totalMemory() + "</p>";
+		answer += "<p>" + "Free memory (bytes): " + Runtime.getRuntime().freeMemory() + "</p>";
+		answer += "<p>" + "available Processors : " + Runtime.getRuntime().availableProcessors() + "</p>";
 		answer += "</body>";
 		answer += "</table></body></html>";
-	
-		int counter=1;   
+
 		try (ServerSocket soc = new ServerSocket(8080)) {
-			
+			int counter = 0;
 			for (;;) {
-				 				 Socket clisoc = soc.accept();	
-				 			//	 clisoc.
-				 String rn="Request number:"+counter;
-				 Client cli = new Client(clisoc, answer+rn);
-				 counter=counter+1;
-				 
+				Socket clisoc = soc.accept();
+				counter = counter + 1;
+				String rn = "Request number:" + counter;
+				Client cli = new Client(clisoc, answer + rn);
+				
+
 			}
 		} catch (IOException e) {
 			System.out.println("Error to server Socket Open!!!");
 		}
-	
+
 	}
 }
